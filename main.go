ed0e3f82b6cfbd98237d8d399cc0f7b6ec05a227
霍帅兵 <shuaibing1@staff.sina.com.cn>
2020-12-11 18:16:27 +0800

    2

--- main.go
+++ main.go
@@ -9,56 +9,19 @@ import (
 	"github.com/docker/docker/client"
 )
 
-// func main() {
-// 	ips, err := net.InterfaceByName("eth0")
-// 	if err != nil {
-// 		log.Fatalln(err)
-// 	}
-// 	addrs, err := ips.Addrs()
-// 	if err != nil {
-// 		log.Fatalln()
-// 	}
-// 	fmt.Println(addrs[0])
-// 	fmt.Println(addrs[0].String())
-// 	fmt.Println(reflect.TypeOf(addrs[0].String()))
-// }
-// type info map[string]string
-
-//Deal is a func
-// func Deal() (int, []info) {
-// 	// var number int
-// 	infoins := make(info)
-// 	con_list := make([]info)
-// 	cli, err := client.NewEnvClient()
-// 	if err != nil {
-// 		logrus.Errorln("init a new API client err")
-// 	}
-// 	containers, _ := cli.ContainerList(context.Background(), types.ContainerListOptions{
-// 		Size:    true,
-// 		All:     true,
-// 		Since:   "container",
-// 		Filters: filters.Args{},
-// 	})
-// 	var number int = len(containers)
-// 	for _, con := range containers {
-// 		name := con.Names[0][1:]
-// 		infoins[name] = con.State
-// 		con_list = append(con_list, infoins)
-// 	}
-// 	return number, con_list
-// }
 func main() {
-	// num, li := Deal()
-	// fmt.Println(num)
-	// fmt.Println(li)
+	fmt.Println("11111")
 	cli, _ := client.NewEnvClient()
+	fmt.Println("222")
 	containers, _ := cli.ContainerList(context.Background(), types.ContainerListOptions{
 		Size:    true,
 		All:     true,
 		Since:   "container",
 		Filters: filters.Args{},
 	})
+	fmt.Println(containers)
 	for _, con := range containers {
+		fmt.Println("444")
 		fmt.Println("test=======")
 		fmt.Println(con.Names[0])
 		fmt.Println(con.State)
