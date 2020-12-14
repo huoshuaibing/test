b5b1ebe0fde87986ee0404985db9768713bf4079
霍帅兵 <shuaibing1@staff.sina.com.cn>
2020-12-14 18:00:49 +0800

    3

--- main.go
+++ main.go
@@ -9,22 +9,28 @@ import (
 	"github.com/docker/docker/client"
 )
 
-func main() {
-	fmt.Println("11111")
-	// cli, _ := client.NewEnvClient()
-	cli, _ := client.NewClientWithOpts(client.FromEnv)
-	fmt.Println("222")
+type info map[string]string
+
+func Deal() (int, []info) {
+	infoins := make(info)
+	con_list := make([]info, 100)
+	cli, _ := client.NewEnvClient()
 	containers, _ := cli.ContainerList(context.Background(), types.ContainerListOptions{
 		Size:    true,
 		All:     true,
 		Since:   "container",
 		Filters: filters.Args{},
 	})
-	fmt.Println(containers)
+	var number int = len(containers)
 	for _, con := range containers {
-		fmt.Println("444")
-		fmt.Println("test=======")
-		fmt.Println(con.Names[0])
-		fmt.Println(con.State)
+		name := con.Names[0][1:]
+		infoins[name] = con.State
+		con_list = append(con_list, infoins)
 	}
+	return number, con_list
+}
+func main() {
+	nu, li := Deal()
+	fmt.Println(nu)
+	fmt.Println(li)
 }
