eede4b53beafbcf17870c8fea1704c9d96270b35
霍帅兵 <shuaibing1@staff.sina.com.cn>
2021-01-04 15:27:24 +0800

    2

--- main.go
+++ main.go
@@ -3,34 +3,48 @@ package main
 import (
 	"fmt"
 	"net"
-	"os"
 	"strings"
 
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	ips, err := net.InterfaceByName("eth0")
+	// ips, err := net.InterfaceByName("eth0")
+	// fmt.Println(ips)
+	// if err != nil {
+	// 	ips, err = net.InterfaceByName("eth1")
+	// 	if err != nil {
+	// 		ips, err = net.InterfaceByName("eth2")
+	// 		if err != nil {
+	// 			ips, err = net.InterfaceByName("eth3")
+	// 			if err != nil {
+	// 				ips, err = net.InterfaceByName("eth4")
+	// 			}
+	// 		}
+	// 	}
+	// }
+	// addrs, err := ips.Addrs()
+	// fmt.Println(addrs)
+	// if err != nil {
+	// 	logrus.Fatalln(err)
+	// }
+	// name, err := os.Hostname()
+	// if err != nil {
+	// 	logrus.Fatalln("Get Hostname failed", err)
+	// }
+	// fmt.Println(strings.Split(addrs[0].String(), "/")[0], name)
+	interfaces, err := net.Interfaces()
 	if err != nil {
-		ips, err = net.InterfaceByName("eth1")
-		if err != nil {
-			ips, err = net.InterfaceByName("eth2")
+		logrus.Errorln("can not get local interface")
+	}
+	for _, inter := range interfaces {
+		flags := inter.Flags.String()
+		if strings.Contains(flags, "up") && strings.Contains(flags, "broadcast") {
+			li, err := inter.Addrs()
 			if err != nil {
-				ips, err = net.InterfaceByName("eth3")
-				if err != nil {
-					ips, err = net.InterfaceByName("eth4")
-				}
+				logrus.Errorln(err)
 			}
+			fmt.Println(li)
 		}
 	}
-	addrs, err := ips.Addrs()
-	fmt.Println(addrs)
-	if err != nil {
-		logrus.Fatalln(err)
-	}
-	name, err := os.Hostname()
-	if err != nil {
-		logrus.Fatalln("Get Hostname failed", err)
-	}
-	fmt.Println(strings.Split(addrs[0].String(), "/")[0], name)
 }
